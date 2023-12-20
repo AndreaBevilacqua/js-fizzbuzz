d8d2b4f666f1465243d5cf2d63e31f8cc86c1977
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-20 15:21:36 +0100

    added todo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* 
+  1 - Definiamo la variabile i e diamo il valore 1
+  2 - Ciclo for, si ripete fino a quando i é minore o uguale a 100
+  3 - Verifica condizione FizzBuzz, se il numero é un multipo sia di 3 che di 5
+  4 - Verifica condizione Fizz, se il numero é multiplo di 3
+  5 - Verifica condizione Buzz, se il numero é multiplo di 5
+  6 - Se non é multiplo di 3 e 5 stampa il numero stesso in console
+*/
---
+++ readme.md
@@ -0,0 +1,9 @@
+Ciao ragazzi,
+Esercizio di oggi: FizzBuzz
+nome repo: js-fizzbuzz
+Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero.
+per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
